63ac5104a028b134b8b60854022fc8c71d86a919
salihe <salihe.bodeva10@gmail.com>
2024-05-31 23:16:28 +0300

    Users- almost done

---
+++ Architecture/Architecture.Common/GlobalConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Architecture.Common
+{
+    public class GlobalConstants
+    {
+        public const string AdminRole = "Admin";
+        public const string UserRole = "User";
+        public const string DefaultImage = "-";
+    }
+}
