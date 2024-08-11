8a98789795fc8e410f8280b2b0c45aead317b295
salihe <salihe.bodeva10@gmail.com>
2024-08-11 12:54:12 +0300

    Common data done

---
+++ Architecture/Architecture.Common/GlobalConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Architecture.Common
+{
+    public class GlobalConstants
+    {
+        public const string EmployeeRole = "Employee";
+        public const string ClientRole = "Client";
+        public const string Admin = "Admin";
+        public const string DefaultImage = "-";
+    }
+}
