a7440914c55d14838a49eea2b136cb4d0021fae3
salihe <salihe.bodeva10@gmail.com>
2024-08-12 18:27:05 +0300

    Dealing with users...

---
+++ Architecture/Architecture.Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using Architecture.Data;
+using Architecture.Data.Models;
+using Architecture.Services.Contracts;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Architecture.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<User> userManager;
+        private readonly ApplicationDbContext context;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly SignInManager<User> signInManager;
+        private const int ItemsCount = 0;
+
+        public UserService(UserManager<User> userManager, ApplicationDbContext context, RoleManager<IdentityRole> roleManager, SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.context = context;
+            this.roleManager = roleManager;
+            this.signInManager = signInManager;
+        }
+
+    }
+}
